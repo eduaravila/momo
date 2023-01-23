88d99600d45d0e773ef86e30e79fbe5a6799b2b8
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2023-01-22 21:22:51 -0600

    feat(turbo): split env file for web app, add env dependencies

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -1,15 +1,14 @@
 
 import { appLinks } from "@utils/links";
 import Link from "next/link";
-import { Button } from "ui";
 
 
 export default function Web() {
+  
   return (
     <div>
       <h1>Web</h1>
-      <Link href={appLinks}>Login with twitch</Link>
-      <Button> <span>Login with twith</span></Button>
+      <Link href={appLinks.twithAuth}>Login with twitch</Link>      
     </div>
   );
 }
--- apps/dashboard/utils/links.ts
+++ apps/dashboard/utils/links.ts
@@ -1,5 +1,6 @@
 
-const twitchAuthLink = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_TWITCH_REDIRECT_URI}&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state`
+const twitchAuthLink = `https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=${process.env.NEXT_PUBLIC_TWITCH_APPLICATION_CLIEND_ID}&redirect_uri=${process.env.NEXT_PUBLIC_TWITCH_APPLICATION_REDIRECT_URI}&scope=${process.env.NEXT_PUBLIC_TWITCH_APPLICATION_SCOPES}`
+
 
 export const appLinks = {
   twithAuth: twitchAuthLink,
