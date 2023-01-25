64e88ac4f17a274a43a9dc5c830bd026ee6280dd
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2023-01-25 02:09:10 -0600

    feat: fallback erro code

--- apps/listener/utils/handler.go
+++ apps/listener/utils/handler.go
@@ -4,8 +4,8 @@ import "net/http"
 
 func Handle(method string, route string, handler func(w http.ResponseWriter, r *http.Request), mux *http.ServeMux) {
 	mux.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == method {
-			handler(w, r)
+		if r.Method != method {
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 			return
 		}
 		handler(w, r)
