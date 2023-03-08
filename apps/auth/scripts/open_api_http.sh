7bb23c4ad73d18b8fca74c9f28d4b59bfc5c27a9
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2023-03-07 19:31:01 -0600

    feat: (BREAKING): change the structure of the project reflect DDD, and CQRS
    Signed-off-by: Eduardo Avila <jesus.amancilla@alumnos.udg.mx>

--- apps/auth/scripts/open_api_http.sh
+++ apps/auth/scripts/open_api_http.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# This sets the script to exit immediately if any command in the script fails with a non-zero exit code.
+set -e
 
 readonly service = "$1"
-readonly output_dir = "$2"
 readonly package = "$3"
+readonly output_dir = "$3"
 
-oapi-codegen --package="$package" -generate chi-server "$service".yaml > api.gen.go
-oapi-codegen --package="$package" -generate types auth.yaml > api_types.gen.go
+
+oapi-codegen  -generate chi-server -package "$package" -o "$output_dir/openapi_api.gen.go" api/openapi/"$service".yaml
+
+oapi-codegen -generate types -package "$package" -o "$output_dir/openapi_types.gen.go" "api/openapi/$service".yaml
