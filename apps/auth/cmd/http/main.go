17e51d44e1d23b364c0676d1ae316367905234fb
Eduardo Avila <contact@eduaravila.com>
2023-04-06 23:40:19 -0600

    feat (momo): add file to query validity of session token
    Signed-off-by: Eduardo Avila <contact@eduaravila.com>

--- apps/auth/cmd/http/main.go
+++ apps/auth/cmd/http/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
@@ -9,7 +10,13 @@ import (
 
 func main() {
 	port := os.Getenv("AUTH_PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
 	server.RunHTTPServer("/api", port, func() http.Handler {
+		log.Default().Println("Listening on port", port)
 		return http.NewServeMux()
 	})
+
 }
--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -3,6 +3,15 @@ import { appLinks } from "@utils/links";
 import Link from "next/link";
 
 
+export const getServerSideProps = async ()=> {
+  console.log(process.env.NEXT_PUBLIC_AUTH_SERVER_API_URL);
+
+  return {
+    props: {}
+  }
+  
+}
+
 export default function Web() {
   
   return (
