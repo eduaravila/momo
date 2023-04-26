fa712e1cffe25d5791f69a775ea0289278b3f788
Eduardo Avila <contact@eduaravila.com>
2023-04-25 18:24:27 -0600

    add integration test for session (postgres) storage 	add testify lib to clean test cases 	add test to get or create user

---
+++ apps/auth/internal/storage/oauth_postgres_storage_test.go
@@ -0,0 +1,84 @@
+package storage_test
+
+import (
+	"math/rand"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/eduaravila/momo/apps/auth/internal/domain/session"
+	"github.com/eduaravila/momo/apps/auth/internal/storage"
+	"github.com/eduaravila/momo/packages/postgres/queries"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+var userAgents = []string{
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+	"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1",
+	"Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) FxiOS/8.1.1b4948 Mobile/14F89 Safari/603.2.4",
+	"Mozilla/5.0 (iPad; CPU OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1",
+	"Mozilla/5.0 (Linux; Android 4.3; GT-I9300 Build/JSS15J) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36",
+	"Mozilla/5.0 (Android 4.3; Mobile; rv:54.0) Gecko/54.0 Firefox/54.0",
+	"Mozilla/5.0 (Linux; Android 4.3; GT-I9300 Build/JSS15J) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36 OPR/42.9.2246.119956",
+	"Opera/9.80 (Android; Opera Mini/28.0.2254/66.318; U; en) Presto/2.12.423 Version/12.16",
+}
+
+func TestGetOrCreateUserFromSubPostgres(t *testing.T) {
+
+}
+
+func newExampleClaims(userID string) *session.Claims {
+	return session.NewClaims(
+		os.Getenv("JWT_ISSUER"),
+		userID,
+		time.Now().Add(time.Hour*24*30),
+		time.Now(),
+		time.Now())
+}
+
+func newExampleSession(t *testing.T) *session.Session {
+	userID := uuid.NewString()
+
+	token, err := session.NewSessionToken(
+		uuid.NewString(),
+		true,
+		newExampleClaims(userID),
+	)
+
+	require.NoError(t, err)
+	session, err := session.NewSession(
+		uuid.NewString(),
+		userID,
+		time.Now(),
+		token,
+		userAgents[rand.Intn(len(userAgents))],
+		newExampleIP(),
+	)
+	require.NoError(t, err)
+
+	return session
+}
+
+func newExampleIP() string {
+	rand.Seed(time.Now().UnixNano())
+	ip := net.IPv4(
+		byte(rand.Intn(255)),
+		byte(rand.Intn(255)),
+		byte(rand.Intn(255)),
+		byte(rand.Intn(255)),
+	)
+
+	return ip.String()
+}
+
+func newSessionPostgresStorage(t *testing.T) *storage.OauthPostgresStorage {
+	t.Helper()
+
+	db, err := storage.InitPostgresDB()
+	require.NoError(t, err)
+
+	return storage.NewSessionPostgresStorage(queries.New(db))
+}
--- go.mod
+++ go.mod
@@ -12,12 +12,17 @@ require (
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deepmap/oapi-codegen v1.12.4 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
